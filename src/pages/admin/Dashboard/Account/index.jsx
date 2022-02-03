81c0b2c5d68e03dfe483d0d58c8b7d30721eef92
Raji Mustapha <rajimustapha30@gmail.com>
2022-02-03 13:25:03 +0100

    fix

--- src/pages/admin/Dashboard/Account/index.jsx
+++ src/pages/admin/Dashboard/Account/index.jsx
@@ -8,18 +8,23 @@ import { useState } from "react";
 import Modal from "../../../../component/Modal";
 import FormHead from "../../../../component/formHead";
 import Input from "../../../../component/input";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import SubmitBtn from "../../../../component/submitBtn";
 import Select from "../../../../component/input/selectt";
 import SuccessModal from "../../../../component/popModal";
 import TableDrop from "../../../../component/TableDrop";
 import Phone from "../../../../component/input/phone";
+import { ReactComponent as Loka } from "../../../../assets/icons/loka.svg";
+import { ReactComponent as Circ } from "../../../../assets/icons/circ.svg";
+import InputField from "../../../../component/input/indexField";
 const Account = () => {
   const list = [1, 2, 3];
   const [modal, setModal] = useState(false);
   const [modalPop, setModalPop] = useState(false);
-  const { register, formState, handleSubmit } = useForm();
+  const methods = useForm();
+  const methods2 = useForm();
   const [isLoadng, setIsLoading] = useState(false);
+  const [phone, setPhone] = useState("");
   const closeModal = () => {
     setModal(!modal);
   };
@@ -28,20 +33,20 @@ const Account = () => {
   };
 
   const onSubmit = (vals) => {
+    const payload = {
+      ...vals,
+      phone: phone,
+    };
+    console.log(payload);
+    setIsLoading(false);
+  };
+
+  const onSubmit2 = (vals) => {
     console.log(vals);
     setIsLoading(false);
   };
-  const [phone, setPhone] = useState("");
-  const roleOption = [
-    {
-      label: "None Financial",
-      value: "none",
-    },
-    {
-      label: "None Financial2",
-      value: "none2",
-    },
-  ];
+
+  const [focus, setFocus] = useState("profile");
   return (
     <AdminDashboardLayout active="account">
       <div className="pd-account">
@@ -55,105 +60,118 @@ const Account = () => {
           <p className="pageTitle">Account Setting</p>
         </div>
 
-        <div className="whiteContainer">
-          <div className="coverSetting">
-            <div className="left">
-              <p className="leftHead">Change Password</p>
-              <form action="">
-                {/* <Input
-                  type="email"
-                  name="email"
-                  placeholder="Enter email address"
-                  label="Email"
-                  id="email"
-                  register={register}
-                  errors={formState.errors}
-                  errMsg="invalid email input"
-                /> */}
-
-                <Input
-                  type="password"
-                  name="password"
-                  label="Current Password"
-                  placeholder="Current Password"
-                  id="password"
-                  register={register}
-                  errors={formState.errors}
-                  errMsg="invalid password"
-                />
-                <Input
-                  type="password"
-                  name="password"
-                  label="New Password"
-                  placeholder="New Password"
-                  id="password"
-                  register={register}
-                  errors={formState.errors}
-                  errMsg="invalid password"
-                />
-
-                <Input
-                  type="password"
-                  name="password"
-                  label="Confirm Password"
-                  placeholder="Confirm Password"
-                  id="password"
-                  register={register}
-                  errors={formState.errors}
-                  errMsg="invalid password"
-                />
-                <SubmitBtn isLoading={isLoadng} btnText="Save" />
-              </form>
+        <div className="flexPart">
+          <div className="toggleBox">
+            <div
+              onClick={() => {
+                setFocus("profile");
+              }}
+              className={`eachList ${focus === "profile" ? "active" : ""}`}
+            >
+              <Circ className="iconSide" />
+              <p>Profile Details</p>
             </div>
-            <div className="left">
-              <p className="leftHead">Profile Details</p>
-              <form action="">
-                <div className="group">
-                  <div className="halfInput">
-                    <Input
-                      type="text"
-                      name="firstname"
-                      placeholder="Firstname"
-                      label="Firstname"
-                      id="firstname"
-                      register={register}
-                      errors={formState.errors}
-                      errMsg="invalid input"
-                    />
-                  </div>
-                  <div className="halfInput">
-                    <Input
-                      type="text"
-                      name="lastname"
-                      placeholder="Lastname"
-                      label="Lastname"
-                      id="lastname"
-                      register={register}
-                      errors={formState.errors}
-                      errMsg="invalid input"
-                    />
-                  </div>
-                </div>
-                <Input
-                  type="email"
-                  name="email"
-                  placeholder="Enter email address"
-                  label="Email"
-                  id="email"
-                  register={register}
-                  errors={formState.errors}
-                  errMsg="invalid email input"
-                />
-
-                <Phone
-                  label={"Mobile no"}
-                  telVal={phone}
-                  setTelVal={setPhone}
-                />
-
-                <SubmitBtn isLoading={isLoadng} btnText="Update" />
-              </form>
+            <div
+              onClick={() => {
+                setFocus("password");
+              }}
+              className={`eachList ${focus === "password" ? "active" : ""}`}
+            >
+              <Loka className="iconSide" />
+              <p>Password Setting</p>
             </div>
+          </div>
+
+          <div className="whiteGuy">
+            {focus === "profile" && (
+              <div className="coverForm">
+                <FormProvider {...methods}>
+                  <form
+                    onSubmit={methods.handleSubmit(onSubmit)}
+                    // className="loginBox2"
+                    action=""
+                  >
+                    <FormHead title={"Profile Details"} />
+
+                    <div className="group">
+                      <div className="halfInput">
+                        <InputField
+                          type="text"
+                          name="firstname"
+                          placeholder="Firstname"
+                          label="Firstname"
+                          id="firstname"
+                          errMsg="invalid input"
+                        />
+                      </div>
+                      <div className="halfInput">
+                        <InputField
+                          type="text"
+                          name="lastname"
+                          placeholder="Lastname"
+                          label="Lastname"
+                          id="lastname"
+                          errMsg="invalid input"
+                        />
+                      </div>
+                    </div>
+
+                    <InputField
+                      type="email"
+                      name="email"
+                      placeholder="Enter email address"
+                      label="Email"
+                      id="email"
+                      errMsg="invalid email input"
+                    />
+                    <Phone
+                      label={"Mobile no"}
+                      telVal={phone}
+                      setTelVal={setPhone}
+                    />
+
+                    <SubmitBtn isLoading={false} btnText="Update" />
+                  </form>
+                </FormProvider>
+              </div>
+            )}
+            {focus === "password" && (
+              <div className="coverForm">
+                <FormProvider {...methods2}>
+                  <form onSubmit={methods2.handleSubmit(onSubmit2)} action="">
+                    <FormHead title={"Profile Details"} />
+
+                    <InputField
+                      type="password"
+                      name="password"
+                      label="Current Password"
+                      placeholder="Current Password"
+                      id="password"
+                      errMsg="invalid password"
+                    />
+                    <InputField
+                      type="password"
+                      name="new_password"
+                      label="New Password"
+                      placeholder="New Password"
+                      id="password"
+                      errMsg="invalid password"
+                    />
+
+                    <InputField
+                      type="password"
+                      name="con_password"
+                      label="Confirm Password"
+                      placeholder="Confirm Password"
+                      id="password"
+                      errMsg="invalid password"
+                    />
+
+                    <SubmitBtn isLoading={false} btnText="Update" />
+                  </form>
+                </FormProvider>
+              </div>
+            )}
           </div>
         </div>
       </div>
